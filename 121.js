9929e63425e15946aa48ba6d0125c555fc99c4f8
DanilLaktionov <laktionov_88@icloud.com>
2023-03-02 06:06:23 +0200

    js

---
+++ 121.js
@@ -0,0 +1,11 @@
+name = prompt("Как тебя зовут?");
+alert('Привет,' +name+ ' :)');
+
+age = prompt("Год твоего рождения?");
+let old =(2023 - age);
+alert('Тебе ' +old+ ' лет.');
+
+square = prompt("Какая длина стороны квадрата?");
+let multiplication = ( square * 4);
+alert('Периметр этого квадрата равен ' +multiplication+ ' см')
+ 
